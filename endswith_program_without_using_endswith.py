e1528ce1b69893a5dae5316b6e0365cd82172807
unknown <sainttrowagmail.com>
2025-03-28 14:33:47 +0800

    ASk the user to input a suffix

--- endswith_program_without_using_endswith.py
+++ endswith_program_without_using_endswith.py
@@ -2,4 +2,6 @@
 user_input = str(input("Please input your name or a sentence: "))
 
 #Ask the user to input a suffix and check if the previous input ends with it
+suffix_input = str(input("Please input your suffix: "))
+
 #Print "True" if the suffix is there and "False" if not
