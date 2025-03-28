65364f36fa00446084ccd4ecaa078764d0b773ca
unknown <sainttrowagmail.com>
2025-03-28 13:03:56 +0800

    Print the name without the spaces and without using lstrip

--- lstrip_program_without_using_lstrip.py
+++ lstrip_program_without_using_lstrip.py
@@ -1,3 +1,12 @@
 #Ask the user to input their name with multiple spaces in the beginning
 name_input = str(input("Please input your name with several spaces at the beginning (     Trowa Tangco): "))
+
 #Print the name without the spaces and without using lstrip
+def leftstrip(name_input):
+    for i in range(len(name_input)):
+        if name_input[i] != " ":
+         return name_input[i:]
+    
+    return ""
+
+print(leftstrip(name_input))
