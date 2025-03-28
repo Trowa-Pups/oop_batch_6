e58e35d24678cc45205fd844cd560790a2b5ec3f
unknown <sainttrowagmail.com>
2025-03-28 17:13:26 +0800

    ask the user to input their name or a sentence

--- center_program_without_using_center.py
+++ center_program_without_using_center.py
@@ -1,4 +1,6 @@
 #Ask the user to input their name or a sentence
+user_input = str(input("Please input your name or a sentence: "))
+
 #Ask the user the width of the text 
 #Calculate the padding on both sides to the center
 #Print the input in the center within the given width
