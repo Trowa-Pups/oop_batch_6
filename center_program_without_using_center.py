4e3d9eaeebea5274a35a9e9a4fb984994130bf8e
unknown <sainttrowagmail.com>
2025-03-28 17:23:16 +0800

    Calculate the padding on both sides to the center

--- center_program_without_using_center.py
+++ center_program_without_using_center.py
@@ -5,4 +5,7 @@ user_input = str(input("Please input your name or a sentence: "))
 width = int(input("Please input the width of the text: "))
 
 #Calculate the padding on both sides to the center
+left_padding = (width - len(user_input)) // 2 #Uses //2 so that it doesnt output something like 7.5 
+right_padding = ((width - len(user_input))) - left_padding 
+                 
 #Print the input in the center within the given width
