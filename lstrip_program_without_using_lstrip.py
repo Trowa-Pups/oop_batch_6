a7af38f128f090488c2d7ee509876d9a80f25e9f
unknown <sainttrowagmail.com>
2025-03-28 12:53:01 +0800

    ask the user to input their name with multiple spaces in the beginning

--- lstrip_program_without_using_lstrip.py
+++ lstrip_program_without_using_lstrip.py
@@ -1,2 +1,3 @@
 #Ask the user to input their name with multiple spaces in the beginning
+name_input = str(input("Please input your name with several spaces at the beginning (     Trowa Tangco): "))
 #Print the name without the spaces and without using lstrip
