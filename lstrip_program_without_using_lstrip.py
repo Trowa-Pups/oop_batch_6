8869359de0006d694c04ce743b1cc24d0f05d6a0
unknown <sainttrowagmail.com>
2025-03-28 13:20:46 +0800

    Comment added

--- lstrip_program_without_using_lstrip.py
+++ lstrip_program_without_using_lstrip.py
@@ -3,9 +3,9 @@ name_input = str(input("Please input your name with several spaces at the beginn
 
 #Print the name without the spaces and without using lstrip
 def leftstrip(name_input):
-    for i in range(len(name_input)):
-        if name_input[i] != " ":
-         return name_input[i:]
+    for i in range(len(name_input)): 
+        if name_input[i] != " ": #To check if its not a space
+         return name_input[i:] #To keep the name input without the zero
     
     return ""
 
