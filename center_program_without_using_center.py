ac000c7c95d82b601e9c83b1b860f15b76fc691a
unknown <sainttrowagmail.com>
2025-03-28 17:12:16 +0800

    pseudocode

---
+++ center_program_without_using_center.py
@@ -0,0 +1,4 @@
+#Ask the user to input their name or a sentence
+#Ask the user the width of the text 
+#Calculate the padding on both sides to the center
+#Print the input in the center within the given width
