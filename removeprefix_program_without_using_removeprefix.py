14a792d06e2b4dd591bc7bf20dc3fb6e0ae7a399
unknown <sainttrowagmail.com>
2025-03-28 13:22:09 +0800

    Ask the user to input a sentence and input a word to remove in that sentence

--- removeprefix_program_without_using_removeprefix.py
+++ removeprefix_program_without_using_removeprefix.py
@@ -1,2 +1,5 @@
 #Ask the user to input a sentence and input a word to remove in that sentence
+sentence_input = str(input("Please input a sentence: "))
+prefix_input = str(input("Please input a word that you want to remove in the sentence: "))
+
 #Print the sentence without the word that the user wants to remove
