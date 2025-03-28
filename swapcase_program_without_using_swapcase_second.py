e3f22e4cbf6123f3edc693fc0726ac69e53a3156
unknown <sainttrowagmail.com>
2025-03-28 18:05:47 +0800

    Print the input in swapped case without using swapcase()

--- swapcase_program_without_using_swapcase_second.py
+++ swapcase_program_without_using_swapcase_second.py
@@ -2,3 +2,17 @@
 user_input = str(input("Please input your name or a sentence: "))
 
 #Print the input in swappped case without using swapcase()
+swapcased = ""
+
+for character in user_input:
+    if character.isupper():
+        swapcased += character.lower()
+    
+    if character.islower():
+        swapcased += character.upper()
+
+    else:
+        swapcased += character
+
+print(swapcased)
+        
