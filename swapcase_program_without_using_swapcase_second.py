2964d44ba0e49542f0e43cf9ec0e54aacfc20b3c
unknown <sainttrowagmail.com>
2025-03-28 18:26:10 +0800

    comments added

--- swapcase_program_without_using_swapcase_second.py
+++ swapcase_program_without_using_swapcase_second.py
@@ -2,17 +2,17 @@
 user_input = str(input("Please input your name or a sentence: "))
 
 #Print the input in swappped case without using swapcase()
-swapcased = ""
+swapcased = "" #To store the words
 
 for character in user_input:
-    if character.isupper():
-        swapcased += character.lower()
+    if character.isupper(): #Checks if its uppercased
+        swapcased += character.lower() #Converts to lower
     
-    if character.islower():
-        swapcased += character.upper()
+    if character.islower(): #Checks if its lowercased
+        swapcased += character.upper() #Converts to upper
 
     else:
-        swapcased += character
+        swapcased += character #To keep the numbers and special numbers 
 
 print(swapcased)
         
