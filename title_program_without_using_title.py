227007f979933fce976962dd94cdcb7e430c4791
unknown <sainttrowagmail.com>
2025-03-28 18:24:30 +0800

    Print the input into a title() like format without using title()

--- title_program_without_using_title.py
+++ title_program_without_using_title.py
@@ -2,3 +2,8 @@
 user_input = str(input("Please input your name or a sentence: "))
 
 #Print the input into a title() like format without using title()
+titled_input = "" #To store the words that are titled
+for word in user_input.split(): #To split the into words
+    titled_input += word[0].upper() + word[1:].lower() + " "
+
+print(titled_input.rstrip()) #Uses rstrip remove the extra " " at the end
