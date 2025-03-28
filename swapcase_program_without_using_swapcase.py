9d81e6e3a50a9aa793987fe9a1e30dd3e7b8aa0c
unknown <sainttrowagmail.com>
2025-03-28 17:42:20 +0800

    print the input in swapped case without using swapcase()

--- swapcase_program_without_using_swapcase.py
+++ swapcase_program_without_using_swapcase.py
@@ -2,3 +2,18 @@
 user_input = str(input("Please input your name or a sentence: "))
 
 #Print the input in swappped case without using swapcase()
+def swapcase(user_input):
+    swapcased = "" #To store the words
+    for character in user_input:
+        if 'A' <= character <= 'Z': #To check if the character is in uppercase
+            swapcased += chr(ord(character) + 32) #Using chr() and ord() to add 32 into the character in its ASCII value to make it lowercase
+
+        elif 'a' <= character <= 'z': #To check if the character is in lowercase
+            swapcased += chr(ord(character) - 32) #Using chr() and ord() to subtract 32 into the character in its ASCII value to make it upppercase
+
+        else: 
+            swapcased += character #To store numbers or special characters into the swapcased
+        
+    return swapcased
+
+print(swapcase(user_input))
