a7ae23e4541b43917ea96e4103f248a326980926
unknown <sainttrowagmail.com>
2025-03-28 13:40:21 +0800

    print the sentence without the world that the user wants to remove

--- removeprefix_program_without_using_removeprefix.py
+++ removeprefix_program_without_using_removeprefix.py
@@ -1,5 +1,12 @@
-#Ask the user to input a sentence and input a word to remove in that sentence
+#Ask the user to input a sentence and input the first word to remove in that sentence
 sentence_input = str(input("Please input a sentence: "))
-prefix_input = str(input("Please input a word that you want to remove in the sentence: "))
+prefix_input = str(input("Please input the first word that you want to remove in the sentence: "))
 
 #Print the sentence without the word that the user wants to remove
+def remove_prefix(sentence_input, prefix_input):
+    if prefix_input in sentence_input:
+        return sentence_input.replace(prefix_input, "", 1) #Put a 1 in the end to only remove 1 instance of the world
+    
+    return sentence_input
+
+print(remove_prefix(sentence_input, prefix_input))
