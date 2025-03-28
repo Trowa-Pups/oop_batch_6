8f2a629c468b8971719ada805a5fb3f4b348abd1
unknown <sainttrowagmail.com>
2025-03-28 13:51:57 +0800

    Print the input in lowercase without using lower()

--- lower_program_without_using_lower.py
+++ lower_program_without_using_lower.py
@@ -2,3 +2,15 @@
 user_input = str(input("Please input your name or a sentence: "))
 
 #Print the input in lowercase without using lower()
+def lowercase(user_input):
+    lowercased = "" #To store the words
+    for character in user_input:
+        if 'A' <= character <= 'Z': #To check if the character is in uppercase
+            lowercased += chr(ord(character) + 32) #Using chr() and ord() to add 32 into the character in its ASCII value to make it lowercase
+
+        else: 
+            lowercased += character #To store it into the lowercased
+        
+    return lowercased
+
+print(lowercase(user_input))
