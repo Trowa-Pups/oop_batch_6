12d348002b65432657bbdea1aba920a0366f4a98
unknown <sainttrowagmail.com>
2025-03-28 14:02:25 +0800

    Check if the input is upper cased without using isupper()

--- isupper_program_without_using_isupper.py
+++ isupper_program_without_using_isupper.py
@@ -2,4 +2,10 @@
 user_input = str(input("Please input your name or a sentence: "))
 
 #Check if the input is upper cased without using isupper()
+def uppercase_checker(user_input):
+    for character in user_input:
+        if 'A' <= character <= 'Z': #To check if the character is in uppercase
+
+    return uppercase_checker()
+        
 #Print "True" if it is all uppercase and "False" if not
