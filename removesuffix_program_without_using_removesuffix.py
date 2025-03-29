804a3a3afbeaffb7c63e0842c471e274b6a877b2
unknown <sainttrowagmail.com>
2025-03-29 15:34:22 +0800

    PRintthe sentence without the word that the user wants to remove

--- removesuffix_program_without_using_removesuffix.py
+++ removesuffix_program_without_using_removesuffix.py
@@ -5,3 +5,7 @@ sentence_input = str(input("Please input a sentence: "))
 suffix_input = str(input("Please input your suffix to remove it in the sentence: "))
 
 #Print the sentence without the word that the user wants to remove
+if sentence_input.endswith(suffix_input):
+    sentence_input = sentence_input.replace(suffix_input, "", 1)
+
+print(sentence_input)
