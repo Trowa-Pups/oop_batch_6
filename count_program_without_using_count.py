294f79888fa63f92c48f80bed3f2acdcacd8b133
unknown <sainttrowagmail.com>
2025-03-29 19:53:27 +0800

    Print the count of the word inputted to count

--- count_program_without_using_count.py
+++ count_program_without_using_count.py
@@ -2,6 +2,14 @@
 user_input = str(input("Please input your name or a sentence: "))
 
 #Ask the user to input the word to count
-count_input = int(input("Please input the word you want to count: "))
+count_input = str(input("Please input the word you want to count: "))
 
 #Print the count of the word inputted to count
+word_count = 0 #To count how many times the word appears
+
+for word in user_input.split():
+    if word == count_input:
+        word_count += 1
+
+print("Count:" , word_count)
+
