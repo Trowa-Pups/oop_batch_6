133fb96332ad0719366226419f6e6d69b0cba1bc
unknown <sainttrowagmail.com>
2025-03-29 19:47:25 +0800

    ask the user to input the word to count

--- count_program_without_using_count.py
+++ count_program_without_using_count.py
@@ -2,4 +2,6 @@
 user_input = str(input("Please input your name or a sentence: "))
 
 #Ask the user to input the word to count
+count_input = int(input("Please input the word you want to count: "))
+
 #Print the count of the word inputted to count
