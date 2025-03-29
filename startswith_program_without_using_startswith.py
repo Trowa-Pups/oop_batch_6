7797c4468357deb07388c237096b7837a80d7817
unknown <sainttrowagmail.com>
2025-03-29 16:42:13 +0800

    ask the user to input their name or a sentence

--- startswith_program_without_using_startswith.py
+++ startswith_program_without_using_startswith.py
@@ -1,3 +1,5 @@
 #Ask the user to input their name or a sentence
+user_input = str(input("Please input your name or a sentence: "))
+
 #Ask the user to input a prefix and check if the previous input starts with it
 #Print "True" if the prefix is there and "False" if not
