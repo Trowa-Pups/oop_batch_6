2149dbf7010a913f2e4cb0a87a5a8ace8885ae6d
unknown <sainttrowagmail.com>
2025-03-29 19:30:09 +0800

    Ask the user to input a number

--- zfill_program_without_using_zfill.py
+++ zfill_program_without_using_zfill.py
@@ -1,3 +1,5 @@
 #Ask the user to input a number
+number_input = int(input("Please input a number: "))
+
 #Ask the user how many digits Ex. ( 6 (000000) )
 #Print the number in how many digits the user inputed
