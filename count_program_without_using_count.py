442ccc472ab790842335dae2f41655ded5b1e64c
unknown <sainttrowagmail.com>
2025-03-29 19:45:51 +0800

    ask the user to input their name or a sentence

--- count_program_without_using_count.py
+++ count_program_without_using_count.py
@@ -1,3 +1,5 @@
 #Ask the user to input their name or a sentence
+user_input = str(input("Please input your name or a sentence: "))
+
 #Ask the user to input the word to count
 #Print the count of the word inputted to count
