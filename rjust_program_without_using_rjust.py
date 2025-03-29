c2956531ee579a8b3c41f0bfe3f42339290c7312
unknown <sainttrowagmail.com>
2025-03-29 16:58:39 +0800

    ask the user what character and how many is gonna be added

--- rjust_program_without_using_rjust.py
+++ rjust_program_without_using_rjust.py
@@ -2,4 +2,7 @@
 user_input = str(input("Please input your name or a sentence: "))
 
 #Ask the user what character and how many is gonna be added
+character = str(input("Please input what character you want: "))
+number_of_characters = int(input("Please input how many characters: "))
+
 #Print the result 
