841a2531615466a511f9d759f5d1ffa0e5b37e40
unknown <sainttrowagmail.com>
2025-03-29 19:39:07 +0800

    print the number in how many digits the user inputed

--- zfill_program_without_using_zfill.py
+++ zfill_program_without_using_zfill.py
@@ -1,7 +1,10 @@
 #Ask the user to input a number
 number_input = int(input("Please input a number: "))
 
-#Ask the user how many digits Ex. ( 6 (000000) )
+#Ask the user to input how many digits Ex. ( 6 (000000) )
 digit_input = int(input("Please input how many digits Ex. ( 6 (000000) ): "))
 
 #Print the number in how many digits the user inputed
+result = str(number_input).rjust(digit_input, "0") #turned it into str so that rjust to be used to fill the zeros
+
+print (result)
