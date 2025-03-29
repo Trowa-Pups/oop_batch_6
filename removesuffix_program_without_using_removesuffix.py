9b40fa38cac2235a409f17f20c1263ade8f8efee
unknown <sainttrowagmail.com>
2025-03-29 15:26:54 +0800

    Ask the user to input the suffix to remove in that input

--- removesuffix_program_without_using_removesuffix.py
+++ removesuffix_program_without_using_removesuffix.py
@@ -2,4 +2,6 @@
 sentence_input = str(input("Please input a sentence: "))
 
 #Ask the user to input the suffix to remove in that input
+suffix_input = str(input("Please input your suffix to remove it in the sentence: "))
+
 #Print the sentence without the word that the user wants to remove
