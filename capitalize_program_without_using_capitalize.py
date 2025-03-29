59d59694be495dc5a7ee0bca68c271dc59d022c9
unknown <sainttrowagmail.com>
2025-03-29 09:55:59 +0800

    Print the first letter of the input capital letter, and the rest lower case without using capitalize()

--- capitalize_program_without_using_capitalize.py
+++ capitalize_program_without_using_capitalize.py
@@ -2,3 +2,6 @@
 user_input = str(input("Please input your name or a sentence: "))
 
 #Print the first letter of the input capital letter, and the rest small case without using capitalize()
+capitalized_input = user_input[0].upper() + user_input[1:].lower() #Makes the first letter user_input[0], uppercased and the rest lower
+
+print(capitalized_input)
