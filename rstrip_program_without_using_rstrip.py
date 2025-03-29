eb3eb568c38f832425ca6bae08981b1a09bb8269
unknown <sainttrowagmail.com>
2025-03-29 11:12:32 +0800

    Ask the user to input their name with multiple spaces in the end

--- rstrip_program_without_using_rstrip.py
+++ rstrip_program_without_using_rstrip.py
@@ -1,2 +1,4 @@
 #Ask the user to input their name with multiple spaces in the end
+name_input = str(input("Please input your name with several spaces at the end (Trowa Tangco      ): "))
+
 #Print the name without the spaces and without using rstrip
