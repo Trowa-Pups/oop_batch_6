f8d8a7417a56c293d3e7b0c851b29c71e7332d2b
unknown <sainttrowagmail.com>
2025-03-29 16:29:47 +0800

    ask the user to input their name or a sentence

--- islower_program_without_using_lower.py
+++ islower_program_without_using_lower.py
@@ -1,3 +1,5 @@
 #Ask the user to input their name or a sentence
+user_input = str(input("Please input your name or a sentence: "))
+
 #Check if the input is upper cased without using isupper()
 #Print "True" if it is all uppercase and "False" if not
