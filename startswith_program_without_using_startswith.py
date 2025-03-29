23b1cc36b1989ecd13004498fd176e76c2562cda
unknown <sainttrowagmail.com>
2025-03-29 16:41:05 +0800

    pseudocode

---
+++ startswith_program_without_using_startswith.py
@@ -0,0 +1,3 @@
+#Ask the user to input their name or a sentence
+#Ask the user to input a prefix and check if the previous input starts with it
+#Print "True" if the prefix is there and "False" if not
