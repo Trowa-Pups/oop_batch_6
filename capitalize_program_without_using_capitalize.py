642edf1dc2a6553050a17ca6a436716159a6157b
unknown <sainttrowagmail.com>
2025-03-29 09:52:22 +0800

    Ask the user to input their name or a sentence

--- capitalize_program_without_using_capitalize.py
+++ capitalize_program_without_using_capitalize.py
@@ -1,2 +1,4 @@
 #Ask the user to input their name or a sentence
+user_input = str(input("Please input your name or a sentence: "))
+
 #Print the first letter of the input capital letter, and the rest small case without using capitalize()
