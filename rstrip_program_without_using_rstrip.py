3c1d14cf70f53c0688bcc4249de3481ac3dfa920
unknown <sainttrowagmail.com>
2025-03-29 11:51:42 +0800

    Print the name without the spaces without using rstrip

--- rstrip_program_without_using_rstrip.py
+++ rstrip_program_without_using_rstrip.py
@@ -2,3 +2,11 @@
 name_input = str(input("Please input your name with several spaces at the end (Trowa Tangco      ): "))
 
 #Print the name without the spaces and without using rstrip
+def rightstrip(name_input):
+    for i in range(len(name_input)-1, -1, -1): 
+        if name_input[i] != " ": #To check if its not a space
+         return name_input[:i + 1] # I put a +1 in there because it removes the last character not the space
+    
+    return ""
+
+print(rightstrip(name_input))
