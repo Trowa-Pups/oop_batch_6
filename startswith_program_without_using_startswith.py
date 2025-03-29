b397603d95b8623c390664ad31bc6d3f6454dccf
unknown <sainttrowagmail.com>
2025-03-29 16:45:44 +0800

    ask the user to input a prefix and check if the previous input starts with it

--- startswith_program_without_using_startswith.py
+++ startswith_program_without_using_startswith.py
@@ -2,4 +2,13 @@
 user_input = str(input("Please input your name or a sentence: "))
 
 #Ask the user to input a prefix and check if the previous input starts with it
+prefix_input = str(input("Please input your suffix: "))
+
+result = ""
+if user_input[:len(prefix_input):] == prefix_input:
+    result = "True"
+
+else:
+    result = "False"
+
 #Print "True" if the prefix is there and "False" if not
