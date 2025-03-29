023e85577ed29e1ab10227b31c518cca615abde8
unknown <sainttrowagmail.com>
2025-03-29 16:36:51 +0800

    Check if the input is lowercased without using islower(), and Print 'True' if it is all lowercase and 'False' if not

--- islower_program_without_using_lower.py
+++ islower_program_without_using_lower.py
@@ -1,5 +1,16 @@
 #Ask the user to input their name or a sentence
 user_input = str(input("Please input your name or a sentence: "))
 
-#Check if the input is upper cased without using isupper()
-#Print "True" if it is all uppercase and "False" if not
+#Check if the input is lower cased without using islower()
+#Print "True" if it is all lowercase and "False" if not
+result = "" 
+
+for character in user_input:
+    if 'a' <= character <= 'z': #To check if the character is in lowercase
+        result = "True"
+
+    else:
+        result = "False"
+        break #It immediently prints fails when it else is activated
+
+print(result)
