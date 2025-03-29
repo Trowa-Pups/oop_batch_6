aa71fdfa188fcae9084b41e7f132bbcc963a36f0
unknown <sainttrowagmail.com>
2025-03-29 21:28:57 +0800

    Ask the user to input their name or a sentence

--- index_program_without_using_index.py
+++ index_program_without_using_index.py
@@ -1,3 +1,5 @@
 #Ask the user to input their name or a sentence
+user_input = str(input("Please input your name or a sentence: "))
+
 #Ask the user to input a word to find in the input
 #Print the position of the word in the input
