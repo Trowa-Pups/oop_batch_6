971bc30d8dd74011f97ab54b0f2872355aaac668
unknown <sainttrowagmail.com>
2025-03-29 15:41:53 +0800

    Print the input in uppercase without using upper()

--- upper_program_without_using_upper.py
+++ upper_program_without_using_upper.py
@@ -2,3 +2,15 @@
 user_input = str(input("Please input your name or a sentence: "))
 
 #Print the input in uppercase without using upper()
+def uppercase(user_input):
+    uppercased = "" #To store the words
+    for character in user_input:
+        if 'a' <= character <= 'z': #To check if the character is in lowercase
+            uppercased += chr(ord(character) - 32) #Using chr() and ord() to subtract 32 into the character in its ASCII value to make it uppercase
+
+        else: 
+            uppercased += character #To store it into the uppercased
+        
+    return uppercased
+
+print(uppercase(user_input))
