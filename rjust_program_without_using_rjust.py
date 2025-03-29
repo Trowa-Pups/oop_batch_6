8a25ce44f493d84df1f852a2c7fb484f0acd9eef
unknown <sainttrowagmail.com>
2025-03-29 16:55:59 +0800

    ask the user to input their name or a sentence

--- rjust_program_without_using_rjust.py
+++ rjust_program_without_using_rjust.py
@@ -1,3 +1,5 @@
 #Ask the user to input their name or a sentence
+user_input = str(input("Please input your name or a sentence: "))
+
 #Ask the user what character and how many is gonna be added
 #Print the result 
