7436178682472e4242229ff78d7f4b788705d43f
unknown <sainttrowagmail.com>
2025-03-30 10:10:42 +0800

    Print the position of the last occurance of the word in the input

--- rindex_program_without_using_rindex.py
+++ rindex_program_without_using_rindex.py
@@ -5,3 +5,16 @@ user_input = str(input("Please input your name or a sentence: "))
 word_input = str(input("Please input your word: "))
 
 #Print the position of the last occurance of the word in the input
+position = 1 
+last_position = 0 #Made it like found, but it also store the position of the last occurance of the word
+
+for word in user_input.split():
+    if word == word_input: #Checks if the word is the same word that the user ask to find
+        last_position = position 
+    position += 1 #To update the position even if the word isn't found
+
+if last_position <= 0:
+    print("Word not found")
+
+else:
+    print(last_position)
