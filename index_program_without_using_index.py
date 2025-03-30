cb546c4817dfdfb92c311a749ccb7e717b6207eb
unknown <sainttrowagmail.com>
2025-03-30 09:59:24 +0800

    Word not found

--- index_program_without_using_index.py
+++ index_program_without_using_index.py
@@ -6,8 +6,13 @@ word_input = str(input("Please input your word: "))
 
 #Print the position of the word in the input
 position = 1 
+found = 0 #To see if a single word was found
 
 for word in user_input.split():
-    if word == word_input:
-        print(f"Position: {position}")
-    position += 1
+    if word == word_input: #Checks if the word is the same word that the user ask to find
+        print(f"Position: {position}") 
+        found += 1 #To update the found
+    position += 1 #To update the position even if the word isn't found
+
+if found <= 0:
+    print("Word not found")
